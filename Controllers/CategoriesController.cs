42eccfc2e9a7e02124777967bf0d566b24c7c090
omarhatab99 <omarhatab564@gmail.com>
2024-02-21 09:44:10 +0200

    [200~Start Working With Categories Module~

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,80 @@
+﻿using Bookify.Web.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //TODO: use viewModel
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Form");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CategoryFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View("Form", model);
+
+            var category = new Category { Name = model.Name };
+            _context.Add(category);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var category = _context.Categories.Find(id);
+
+            if (category is null)
+                return NotFound();
+
+            var viewModel = new CategoryFormViewModel
+            {
+                Id = id,
+                Name = category.Name
+            };
+
+            return View("Form", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(CategoryFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View("Form", model);
+
+            var category = _context.Categories.Find(model.Id);
+
+            if (category is null)
+                return NotFound();
+
+            category.Name = model.Name;
+            category.LastUpdatedOn = DateTime.Now;
+
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
